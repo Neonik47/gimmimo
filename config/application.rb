1243da0079f993d42e7b0877bdf8c5f700c94d5f
Dmitry <neonik47.1@gmail.com>
2014-10-10 12:38:48 -0700

    mongo alive

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+=begin
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -19,5 +20,83 @@ module Gimmimo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  end
+end
+=end
+
+
+
+require File.expand_path('../boot', __FILE__)
+
+require 'i18n'
+I18n.config.enforce_available_locales = true
+I18n.config.available_locales = [:ru, :en]
+
+require 'rails'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "active_resource/railtie"
+require "rails/test_unit/railtie"
+
+%w(
+  action_controller
+  action_mailer
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
+
+require 'net/http'
+require 'net/https'
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
+
+module Gimmimo
+  class Application < Rails::Application
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/services)
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    if Rails.env == 'test'
+      config.autoload_paths += %W(#{config.root}/spec/dummy/app/controllers)
+    end
+
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    config.time_zone = 'Europe/Moscow'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Enable the asset pipeline
+    config.assets.precompile += ["wordxml.xsl"]
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+    config.generators.helper = false
+
   end
 end
