8f269d921ddea4890bd5591541dcce4eeef65655
Dmitry <neonik47.1@gmail.com>
2014-10-20 16:51:34 -0700

    weather parser

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :services do
+    resources :weather, only: [:index]
+  end
 end
